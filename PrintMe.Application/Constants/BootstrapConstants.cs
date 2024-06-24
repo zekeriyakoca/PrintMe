90def1e5100130a08ba104d04ac1a98c27aa5bad
zekeriya kocairi <zkocairi@skillstown.com>
2024-06-24 22:15:48 +0200

    update frames

--- PrintMe.Application/Constants/BootstrapConstants.cs
+++ PrintMe.Application/Constants/BootstrapConstants.cs
@@ -7,17 +7,126 @@ public static class BootstrapConstants
 {
     public static readonly List<FrameDto> FRAMES = new List<FrameDto>
     {
-        new FrameDto(0, "Rolled-up", "No frame", 0, "https://genstorageaccount3116.blob.core.windows.net/print-me-product-images/no-frame.png"),
-        new FrameDto(1, "Classic Black", "Elegant black frame with a sleek finish, perfect for any artwork.", 3, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(2, "Vintage Gold", "Ornate gold frame with intricate detailing, adds a touch of luxury.", 5, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(3, "Rustic Wood", "Natural wood frame with a rustic charm, ideal for nature prints.", 5, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(4, "Modern Silver", "Contemporary silver frame with a minimalist design.", 6, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(5, "White Wash", "Clean white frame with a distressed finish for a shabby chic look.", 5, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(6, "Matte Black", "Simple matte black frame, versatile for any style of artwork.",4, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(7, "Cherry Wood", "Rich cherry wood frame with a warm, classic feel.", 5, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg"),
-        new FrameDto(8, "Bronze Metallic", "Shiny bronze frame with a metallic finish, adds a modern touch.", 4, "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame-corner-1.jpg")
+        new FrameDto(1, "Black Frame | EDSBRUK",
+            "This traditional, robust frame has a soft profile and comes in many sizes, perfect for a picture wall.",
+            6, "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0723741_pe734158_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame8-edsbruk-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame8-edsbruk.png",
+            "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0723741_pe734158_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0723740_pe734159_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0723739_pe734160_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0723741_pe734158_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__1009335_ph176840_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__0747489_pe744559_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-black-stained__1203360_pe906293_s5.jpg?f=s"
+            }),
+        new FrameDto(2, "White Stained Pine Effect | PLOMMONTRÄD",
+            "The pattern of PLOMMONTRÄD frame has small variations, making each frame unique – and the slightly wider dimensions...",
+            6, "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1202413_pe905936_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame6-plommon-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame6-plommon.png",
+            "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1202413_pe905936_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1202411_pe905935_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1202412_pe905937_s5.jpg?f=xl",
+                "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1202413_pe905936_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1274126_ph194587_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/plommontrad-frame-white-stained-pine-effect__1203594_pe906362_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__1203360_pe906293_s5.jpg?f=s"
+            }),
+        new FrameDto(3, "White Frame | EDSBRUK",
+            "This traditional, robust frame has a soft profile and comes in many sizes, perfect for a picture wall.",
+            6, "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0706506_pe725889_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame5-edsbruk-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame5-edsbruk.png",
+            "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0706506_pe725889_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0706504_pe725890_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0902092_pe725882_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0706506_pe725889_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0939333_ph160793_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__0767914_pe754344_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__1203360_pe906293_s5.jpg?f=s"
+            }),
+        new FrameDto(4, "Brown Frame | RAMSBORG",
+            "Sustainable beauty from sustainably-sourced solid wood, a durable and renewable material that maintains its genuine character with each passing year.", 6,
+            "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0726700_pe735389_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame7-ramsborg-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame7-ramsborg.png",
+            "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0726700_pe735389_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0726699_pe735390_s5.jpg?f=xl",
+                "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0726698_pe735391_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0726700_pe735389_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__1350957_pe951835_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/ramsborg-frame-brown__0919777_ph164133_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/edsbruk-frame-white__1203360_pe906293_s5.jpg?f=s"
+            }),
+        new FrameDto(5, "Black Frame | RÖDALM", "RÖDALM frame has a modern look that does your favourite motifs justice.", 6,
+            "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1251233_pe924195_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame1-rodalm-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame1-rodalm.png",
+            "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1251233_pe924195_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1251232_pe924196_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1298269_pe936170_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1251233_pe924195_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1298276_pe936177_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1298259_pe936159_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/rodalm-frame-black__1330544_pe945716_s5.jpg?f=s"
+            }),
+        new FrameDto(6, "Black Frame | KNOPPANG",
+            "Decorate with pictures you love.",
+            6, "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0638249_pe698799_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame2-knoppang-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame2-knoppang.png",
+            "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0638249_pe698799_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0902477_pe661084_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0902012_pe661072_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0638249_pe698799_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__1187224_pe899111_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__0939314_ph166069_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/knoppang-frame-black__1202391_pe905919_s5.jpg?f=s"
+            }),
+        new FrameDto(7, "Gold Frame | SILVERHÖJDEN",
+            "This frame has a matt metal-like finish and comes in many sizes, perfect for a picture wall.",
+            6, "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1179571_pe895993_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame4-silverhojden-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame4-silverhojden.png",
+            "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1179571_pe895993_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1179570_pe895994_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1179568_pe895991_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1179571_pe895993_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1187227_pe899114_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1350950_pe951830_s5.jpg?f=xl",
+                "https://www.ikea.com/nl/en/images/products/silverhojden-frame-gold-colour__1202816_pe906101_s5.jpg?f=s"
+            }),
+        new FrameDto(8, "Gold Frame | LOMVIKEN",
+            "Decorate with pictures you love. ",
+            6, "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0661072_pe711302_s5.jpg?f=xxs",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame3-lomviken-mat.png",
+            "https://genstorageaccount3116.blob.core.windows.net/printme-images/frame3-lomviken.png",
+            "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0661072_pe711302_s5.jpg?f=s",
+            new string[]
+            {
+                "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0661070_pe711317_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0902049_pe731149_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0661072_pe711302_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__0661071_pe711318_s5.jpg?f=s",
+                "https://www.ikea.com/nl/en/images/products/lomviken-frame-gold-colour__1171372_pe893053_s5.jpg?f=s"
+            })
     };
-    
+
     public static readonly List<SizeDto> SIZES = new List<SizeDto>
     {
         new SizeDto((int)PrintSize.Size13x18, "13x18", "Frame for 13x18 cm prints.", 1),
--- PrintMe.Application/Model/FrameDto.cs
+++ PrintMe.Application/Model/FrameDto.cs
@@ -1,3 +1,13 @@
 namespace PrintMe.Application.Model;
 
-public record FrameDto(int Id, string Name, string Description, long Price, string Thumbnail);
+public record FrameDto(
+    int Id, 
+    string Name, 
+    string Description, 
+    long Price, 
+    string Thumbnail, 
+    string Mask, 
+    string MaskWithoutMat, 
+    string Image, 
+    string[] AllImages
+);
